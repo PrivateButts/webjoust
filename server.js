8ff85a3f9533a8ba4f5bc18409e9da6bf5402274
Randy Butts <randy2@blbcpas.com>
2015-05-05 17:08:50 +0000

    added game view and route. Route requires id or will be forwarded to home

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var http = require('http');
 var express = require('express');
+var jade = require('jade');
 
 var app = express();
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+
 
 app.get('/', function(req, res){
-	res.send("Hello World!");
+	res.render("index")
+})
+
+app.get('/game', function(req, res){
+	res.redirect("/");
+})
+app.get('/game/:id', function(req, res){
+	res.render("game", {id : req.params.id})
 })
 
 app.listen(8080);
